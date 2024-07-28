58e7849546828ef638f4fb91fa4e2d298e056754
poowamin <james.poowamin7@gmail.com>
2024-07-29 01:53:53 +0700

    today Learning about props in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -7,25 +8,47 @@ function genRandomInt(max) {
 }
 
 function Header() {
-    const descripton = reactDescriptions[genRandomInt(2)];
+  const descripton = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {descripton} React concepts you will need for
-        almost any app you are going to build!
+        {descripton} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.descripton}</p>
+    </li>
   );
 }
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              descripton="The core UI building block."
+              image={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
